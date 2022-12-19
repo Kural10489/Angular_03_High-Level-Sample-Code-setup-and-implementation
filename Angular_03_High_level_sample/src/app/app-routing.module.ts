7c3e500f5cb5fd1745ab1024a494db8cf8e5bede
Asplap3275 <kuralanban.karthi@aspiresys.com>
2022-12-19 22:03:10 +0530

    Implemented Routes and Navigations

--- Angular_03_High_level_sample/src/app/app-routing.module.ts
+++ Angular_03_High_level_sample/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { MobilesComponent } from './mobiles/mobiles.component';
+import { DressesComponent } from './dresses/dresses.component';
+const routes: Routes = [
+  {path:'mobiles',component:MobilesComponent },
+  {path:'dresses',component:DressesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents=[MobilesComponent ,DressesComponent]
